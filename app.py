8ef419df511b524046eecfc05c4451d50b04c5e6
watcharap0n <wera.watcharapon@gmail.com>
2021-11-13 18:49:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from routes import callback
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:5000",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://portfolio-watcharapon.herokuapp.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     callback.router,
